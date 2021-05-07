c6cd5461ab242225c64fef8c47b0f2e71ee1ea64
TalesEdu323 <80724397+TalesEdu323@users.noreply.github.com>
2021-05-07 20:32:46 -0300

    .

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
 const cors = require('cors');
 const express = require('express');
 const app = express ();
-const axios = require ('axios')
+const axios = require ('axios');
+const fetch = require('node-fetch');
+const { response } = require('express');
+
+app.use(express.static('public'));
+app.use(express.json());
 
 app.use(cors());
 
-app.get('/' , async(req, res) => {
+app.get('/home' , async(request, response) => {
 
     try {
-        const {data} = await axios ('https://jsonplaceholder.typicode.com/users');
-   console.log(data)
-
-
-    return res.json(data)
-
-    } catch (error) {
-           console.log(erro)
-
+        let data = await fetch('https://jsonplaceholder.typicode.com/users');
+        data = await data.json();
+        return response.json( data )
+        
+    } 
+    catch (error) {
+           console.log(error.message)
     }
     
+});
+
+app.post('/login', async (request, response) => {
+
+    try {
+        const data = request.body
+        return response.json({ status: `Received repository named ${data.repository}, address: ${data.address}, ${data.city}, ${data.state} from frontend` })
+    }
+    catch (err) {
+        return response.json({ error: "Some error" })
+    }
 });
 
 app.listen(8088, () =>{ console.log('Server running on port 8088')});
